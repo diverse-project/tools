c9f7b6d7008a34773d67ae79022f9894df2495d6
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-02-06 17:19:29 +0100

    fix transparency issue when converting png to bmp (was creating an empty file) now also creates parent folder if necessary

--- image-tools/fr.inria.diverse.image-tools.mavenplugin/src/main/java/fr/inria/diverse/image_tools/mavenplugin/ImageConverterMojo.java
+++ image-tools/fr.inria.diverse.image-tools.mavenplugin/src/main/java/fr/inria/diverse/image_tools/mavenplugin/ImageConverterMojo.java
@@ -8,6 +8,8 @@
  */
 package fr.inria.diverse.image_tools.mavenplugin;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -71,8 +73,7 @@ public class ImageConverterMojo extends AbstractMojo {
     /**
      * Output image file
      *
-     * @parameter expression="${project.build.directory}"
-     * @readonly
+     * @parameter
      * @required
      */
     private File output;
@@ -81,7 +82,8 @@ public class ImageConverterMojo extends AbstractMojo {
     /**
      * format of the outputfile
      *
-     * @parameter expression="png"
+     * @parameter 
+     * @required
      */
     private String outputFormat;
     
@@ -99,9 +101,23 @@ public class ImageConverterMojo extends AbstractMojo {
 	        //Read the file to a BufferedImage
 	        BufferedImage image = ImageIO.read(input);
 	
-	
-	        //Write the image to the destination as a BMP
-			ImageIO.write(image, outputFormat, output);
+	        //Write the image to the destination in the correct format
+	        
+	        if(outputFormat.toLowerCase().equals("bmp") || outputFormat.toLowerCase().equals("jpg")|| outputFormat.toLowerCase().equals("jpeg")){
+	        	// remove possible transparent channel
+	        	image = fillTransparentPixels(image, Color.black);
+	        }
+	        output.getParentFile().mkdirs();
+			boolean result = ImageIO.write(image, outputFormat, output);
+			if(!result){
+				this.getLog().error("No appropriate writer found for format "+outputFormat);
+				StringBuilder sb = new StringBuilder();
+				for(String s : ImageIO.getWriterFormatNames()){
+					sb.append(s+", ");
+				}
+				this.getLog().info("Accepted formats: "+sb.toString());
+				
+			}
 		} catch (IOException e) {
 			this.getLog().error(e.getMessage(),e);
 		}
@@ -116,6 +132,18 @@ public class ImageConverterMojo extends AbstractMojo {
 
         }
         return true;
+    }
+    
+    public static BufferedImage fillTransparentPixels( BufferedImage image, Color fillColor ) {
+		int w = image.getWidth();
+		int h = image.getHeight();
+		BufferedImage image2 = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = image2.createGraphics();
+		g.setColor(fillColor);
+		g.fillRect(0,0,w,h);
+		g.drawRenderedImage(image, null);
+		g.dispose();
+		return image2;
     }
         
 }
