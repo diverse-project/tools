38997176b9259ca8706448834dd308b3ec104c7a
Didier Vojtisek <didier.vojtisek@inria.fr>
2015-03-24 16:34:39 +0100

    now the unregister also removes previously dynamically registered EPackage if they comes from the same file

--- commons-eclipse/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/EcoreUnregistering.java
+++ commons-eclipse/org.eclipse.emf.ecoretools.registration/src/org/eclipse/emf/ecoretools/registration/EcoreUnregistering.java
@@ -13,6 +13,9 @@
 
 package org.eclipse.emf.ecoretools.registration;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -28,6 +31,7 @@ public class EcoreUnregistering {
 	/**
 	 * Unregister the contained EPackages by the given IFile
 	 * Security: It ignores request to unregister statically registered EPackages
+	 * In case of change on the IFile, it also unregisters all previously registered packages that comes from the given file 
 	 * @param ecoreFile
 	 * @throws NotValidEPackageURIException 
 	 */
@@ -40,6 +44,22 @@ public class EcoreUnregistering {
 		for(EObject eobj : res.getContents()) {
 			if( eobj instanceof EPackage) {
 				unregisterPackages((EPackage) eobj);
+			}
+		}
+		
+		// search for previously registered EPAckage that comes from a previous version of this file. ie case of a change on the nsURI
+		Collection<Object> packages =  new ArrayList<Object>();
+		packages.addAll(Registry.INSTANCE.values());
+		for(Object pack : packages){
+			if(pack instanceof EPackage){
+				EPackage p = ((EPackage) pack);
+				if(p.eResource() != null && p.eResource().getURI().equals(res.getURI())){
+					try{
+						unregisterPackages(p);
+					} catch (NotValidEPackageURIException e){
+						// ignore because may have been unregistered by recursion
+					}
+				}
 			}
 		}
 	}
