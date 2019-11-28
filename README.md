f05110a020d70cf2513f7d2440c91caefb9154c8
Didier Vojtisek <didier.vojtisek@inria.fr>
2019-11-28 10:38:15 +0100

    cleanup gh-pages
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- README.md
+++
@@ -1,6 +0,0 @@
-tools
-=====
-
-reusable tools that aren't related to any specific team project.
-
-This includes divers-commons tools for Eclipse 
--- commons-eclipse/fr.inria.diverse.commons.eclipse.pde/src/fr/inria/diverse/commons/eclipse/pde/wizards/pages/pde/ui/BaseProjectWizardFields.java
+++
@@ -1,8 +0,0 @@
-package fr.inria.diverse.commons.eclipse.pde.wizards.pages.pde.ui;
-
-public class BaseProjectWizardFields {
-
-	public String projectName;
-	public String projectLocation;
-
-}
